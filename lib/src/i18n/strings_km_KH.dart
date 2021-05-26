db3a7ed27844c8788b4724753ad703b7714e398d
Tith tyratmonipich <tith.tyratmonipich@optistech.com>
2021-05-26 09:03:10 +0700

    [ADDED] khmer locale to calendar

---
+++ lib/src/i18n/strings_km_KH.dart
@@ -0,0 +1,78 @@
+part of 'date_picker_i18n.dart';
+
+/// English (EN) United States
+class _StringsKmKH extends _StringsI18n {
+  const _StringsKmKH();
+
+  @override
+  String getCancelText() {
+    return 'បោះបង់';
+  }
+
+  @override
+  String getDoneText() {
+    return 'ធ្វើរួច';
+  }
+
+  @override
+  List<String> getMonths() {
+    return [
+      "មករា",
+      "កុម្ភៈ",
+      "មិនា",
+      "មេសា",
+      "ឧសភា",
+      "មិថុនា",
+      "កក្តដា",
+      "សីហា",
+      "កញ្ញា",
+      "តុលា",
+      "វិច្ឆិកា",
+      "ធ្នូ"
+    ];
+  }
+
+  @override
+  List<String> getMonthsShort() {
+    return [
+      "មករា",
+      "កុម្ភៈ",
+      "មិនា",
+      "មេសា",
+      "ឧសភា",
+      "មិថុនា",
+      "កក្តដា",
+      "សីហា",
+      "កញ្ញា",
+      "តុលា",
+      "វិច្ឆិកា",
+      "ធ្នូ"
+    ];
+  }
+
+  @override
+  List<String> getWeeksFull() {
+    return [
+      "ចន្ទ",
+      "អង្គារ៏",
+      "ពុធ",
+      "ព្រហស្បតិ៏",
+      "សុក្រ",
+      "សៅរ៏",
+      "អាទិត្យ",
+    ];
+  }
+
+  @override
+  List<String> getWeeksShort() {
+    return [
+      "ចន្ទ",
+      "អង្គារ៏",
+      "ពុធ",
+      "ព្រហ",
+      "សុក្រ",
+      "សៅរ៏",
+      "អាទិ",
+    ];
+  }
+}
